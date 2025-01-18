b46128d14be12f266993aaf0d10d43e9fe517d06
Sterktez <73569420+Sterktez@users.noreply.github.com>
2025-01-18 22:44:38 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+
+registerLink.onclick = () => {
+  wrapper.classList.add("active");
+};
+
+loginLink.onclick = () => {
+  wrapper.classList.remove("active");
+};
